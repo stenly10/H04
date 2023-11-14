d6f325c9742407e2ebcecf8185aae6e2df2f0ec1
stenly10 <yosuasaputra.stenly@gmail.com>
2023-11-14 14:35:00 +0700

    perbaikan lagi (11)

--- custom/H04_2106704244.py
+++ custom/H04_2106704244.py
@@ -45,10 +45,10 @@ class MyTopo( Topo ):
         switch_s = self.addSwitch('s3')
         switch_r = self.addSwitch('s4')
 
-        self.addLink(switch_k, router_rs, intfName2='r0-eth1', params2={'ip':default_gateway_koas})
-        self.addLink(switch_i, router_rs, intfName2='r0-eth2', params2={'ip':default_gateway_internship})
-        self.addLink(switch_s, router_asrama, intfName2='r1-eth1', params2={'ip':default_gateway_spesialis})
-        self.addLink(switch_r, router_asrama, intfName2='r1-eth2', params2={'ip':default_gateway_residen})
+        self.addLink(switch_k, router_rs, intfName1 = 's1-eth1', intfName2='r0-eth1', params2={'ip':default_gateway_koas})
+        self.addLink(switch_i, router_rs, intfName1 = 's2-eth1', intfName2='r0-eth2', params2={'ip':default_gateway_internship})
+        self.addLink(switch_s, router_asrama, intfName1 = 's3-eth1', intfName2='r1-eth1', params2={'ip':default_gateway_spesialis})
+        self.addLink(switch_r, router_asrama, intfName1 = 's4-eth1', intfName2='r1-eth2', params2={'ip':default_gateway_residen})
         self.addLink(router_asrama, router_rs, intfName1 = 'r0-eth3', intfName2='r1-eth3', params1={'ip':asrama_rs}, params2={'ip':rs_asrama})
 
         for switch in ['s1', 's2', 's3', 's4']:
@@ -57,24 +57,24 @@ class MyTopo( Topo ):
                     host_name = f'K{j+1}'
                     ip_addr = f'192.168.244.{j+2}/26'
                     self.addHost(host_name, ip=ip_addr, defaultRoute=f'via {default_gateway_koas}')
-                    self.addLink(host_name, switch)
+                    self.addLink(host_name, switch, intfName2 = f'{switch}-eth{j+2}')
             elif switch == 's2':
                 for j in range(number_host_per_switch_1):
                     host_name = f'I{j+1}'
                     ip_addr = f'192.168.244.{j+66}/27'
                     self.addHost(host_name, ip=ip_addr, defaultRoute=f'via {default_gateway_internship}')
-                    self.addLink(host_name, switch)
+                    self.addLink(host_name, switch, intfName2 = f'{switch}-eth{j+2}')
             elif switch == 's3':
                 for j in range(number_host_per_switch_1):
                     host_name = f'S{j+1}'
                     ip_addr = f'192.168.244.{j+98}/28'
                     self.addHost(host_name, ip=ip_addr, defaultRoute=f'via {default_gateway_spesialis}')
-                    self.addLink(host_name, switch)
+                    self.addLink(host_name, switch, intfName2 = f'{switch}-eth{j+2}')
             else:
                 for j in range(number_host_per_switch_2):
                     host_name = f'R{j+1}'
                     ip_addr = f'192.168.244.{j+114}/28'
                     self.addHost(host_name, ip=ip_addr, defaultRoute=f'via {default_gateway_residen}')
-                    self.addLink(host_name, switch)
+                    self.addLink(host_name, switch, intfName2 = f'{switch}-eth{j+2}')
             
 topos = { 'mytopo': ( lambda: MyTopo() ) }
